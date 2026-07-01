464e0d0c377de04ea935a8ba4b826a00d7bb19ca
muslim-kh09 <hanikhaled876@gmail.com>
2026-07-01 04:13:02 +0000

    Add fallback for Sanity Project ID

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,8 +5,8 @@ import { schemaTypes } from './src/sanity/schemaTypes'
 export default defineConfig({
   name: 'default',
   title: 'Agha Store',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '9sx6xvt7',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   basePath: '/studio',
   plugins: [structureTool()],
   schema: {
